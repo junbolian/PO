2e28b114954b19c608142036a43a85b2390eb0b1
Junbo Lian <101549105+junbolian@users.noreply.github.com>
2024-02-07 10:52:07 +0800

    Add files via upload

---
+++ Parrot-optimizer-Algorithm-and-applications-to-medical-problems-main/func_plot.m
@@ -0,0 +1,126 @@
+%---------------------------------------------------------------------------------------------------------------------------
+% PO 
+% Parrot optimizer: Algorithm and applications to medical problems
+% Website and codes of Parrot optimizer(PO):http://www.aliasgharheidari.com/PO.html
+
+% Junbo Lian, Guohua Hui, Ling Ma, Ting Zhu, Xincan Wu, Ali Asghar Heidari, Yi Chen, Huiling Chen
+
+
+%  Last update: Jan 29 2023
+
+%  e-Mail: as_heidari@ut.ac.ir, aliasghar68@gmail.com, chenhuiling.jlu@gmail.com 
+%  
+%---------------------------------------------------------------------------------------------------------------------------
+%  Authors: Junbo Lian, Ali Asghar Heidari(as_heidari@ut.ac.ir, aliasghar68@gmail.com),Huiling Chen(chenhuiling.jlu@gmail.com) 
+%---------------------------------------------------------------------------------------------------------------------------
+
+% After use of code, please users cite to the main paper on Parrot optimizer (PO):
+% Junbo Lian, Guohua Hui, Ling Ma, Ting Zhu, Xincan Wu, Ali Asghar Heidari, Yi Chen, Huiling Chen
+% Parrot optimizer: Algorithm and applications to medical problems
+%Computers in Biology and Medicine,ELSEVIER- 2024 
+%---------------------------------------------------------------------------------------------------------------------------
+% You can also follow the paper for related updates in researchgate: 
+% https://www.researchgate.net/profile/Ali_Asghar_Heidari.
+
+%  Website and codes of Parrot optimizer (PO):%  http://www.aliasgharheidari.com/PO.html
+
+% You can also use and compare with our other new optimization methods:
+                                                                       %(RIME)-2023-http://www.aliasgharheidari.com/RIME.html
+																	   %(INFO)-2022- http://www.aliasgharheidari.com/INFO.html
+																	   %(RUN)-2021- http://www.aliasgharheidari.com/RUN.html
+                                                                       %(HGS)-2021- http://www.aliasgharheidari.com/HGS.html
+                                                                       %(SMA)-2020- http://www.aliasgharheidari.com/SMA.html
+                                                                       %(HHO)-2019- http://www.aliasgharheidari.com/HHO.html  
+
+%---------------------------------------------------------------------------------------------------------------------------
+
+%---------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+function func_plot(func_name)
+
+[lb,ub,dim,fobj]=Get_Functions_details(func_name);
+
+switch func_name 
+    case 'F1' 
+        x=-100:2:100; y=x; %[-100,100]
+        
+    case 'F2' 
+        x=-100:2:100; y=x; %[-10,10]
+        
+    case 'F3' 
+        x=-100:2:100; y=x; %[-100,100]
+        
+    case 'F4' 
+        x=-100:2:100; y=x; %[-100,100]
+    case 'F5' 
+        x=-200:2:200; y=x; %[-5,5]
+    case 'F6' 
+        x=-100:2:100; y=x; %[-100,100]
+    case 'F7' 
+        x=-1:0.03:1;  y=x  %[-1,1]
+    case 'F8' 
+        x=-500:10:500;y=x; %[-500,500]
+    case 'F9' 
+        x=-5:0.1:5;   y=x; %[-5,5]    
+    case 'F10' 
+        x=-20:0.5:20; y=x;%[-500,500]
+    case 'F11' 
+        x=-500:10:500; y=x;%[-0.5,0.5]
+    case 'F12' 
+        x=-10:0.1:10; y=x;%[-pi,pi]
+    case 'F13' 
+        x=-5:0.08:5; y=x;%[-3,1]
+    case 'F14' 
+        x=-100:2:100; y=x;%[-100,100]
+    case 'F15' 
+        x=-5:0.1:5; y=x;%[-5,5]
+    case 'F16' 
+        x=-1:0.01:1; y=x;%[-5,5]
+    case 'F17' 
+        x=-5:0.1:5; y=x;%[-5,5]
+    case 'F18' 
+        x=-5:0.06:5; y=x;%[-5,5]
+    case 'F19' 
+        x=-5:0.1:5; y=x;%[-5,5]
+    case 'F20' 
+        x=-5:0.1:5; y=x;%[-5,5]        
+    case 'F21' 
+        x=-5:0.1:5; y=x;%[-5,5]
+    case 'F22' 
+        x=-5:0.1:5; y=x;%[-5,5]     
+    case 'F23' 
+        x=-5:0.1:5; y=x;%[-5,5]  
+end    
+
+    
+
+L=length(x);
+f=[];
+
+for i=1:L
+    for j=1:L
+        if strcmp(func_name,'F15')==0 && strcmp(func_name,'F19')==0 && strcmp(func_name,'F20')==0 && strcmp(func_name,'F21')==0 && strcmp(func_name,'F22')==0 && strcmp(func_name,'F23')==0
+            f(i,j)=fobj([x(i),y(j)]);
+        end
+        if strcmp(func_name,'F15')==1
+            f(i,j)=fobj([x(i),y(j),0,0]);
+        end
+        if strcmp(func_name,'F19')==1
+            f(i,j)=fobj([x(i),y(j),0]);
+        end
+        if strcmp(func_name,'F20')==1
+            f(i,j)=fobj([x(i),y(j),0,0,0,0]);
+        end       
+        if strcmp(func_name,'F21')==1 || strcmp(func_name,'F22')==1 ||strcmp(func_name,'F23')==1
+            f(i,j)=fobj([x(i),y(j),0,0]);
+        end          
+    end
+end
+
+surfc(x,y,f,'LineStyle','none');
+
+end
+
